8982ef725c5b84e22145aa38a877951d7dc6d5b2
t-koshi <greapflutea2@yahoo.co.jp>
2017-09-19 09:22:36 +0900

    docker整理

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 起動
 ```
-docker-compose run --service-ports web bundle exec rails s -b 0.0.0.0
+docker-compose run  --rm --service-ports web bundle exec rails s -b 0.0.0.0
 ```
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170909181515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "spots", force: :cascade do |t|
     t.string "state", null: false
